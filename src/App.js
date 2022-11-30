baf35f24310987f3a34fe5684188751abe125e46
Francisco <frandelosantoss12@gmail.com>
2022-11-29 22:43:46 -0300

    Fetch products from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+
 import Navbar from './components/Navbar'
 import Layout from './components/Layout'
 import Title from './components/Title'
@@ -6,29 +7,42 @@ import Products from './components/Products'
 
 class App extends Component {
   state = {
-    products: [
-      {name: 'Tomate', price: 1500, img: 'assets/productos/tomate.jpg'},
-      {name: 'Arbejas', price: 2500, img: 'assets/productos/arbejas.jpg'},
-      {name: 'Lechuga', price: 1500, img: 'assets/productos/lechuga.jpg'}
-    ],
+    loading: true,
+    products: [],
     cart: [],
     cartVisible: false
   }
 
+  async componentDidMount() {
+    const response = await fetch('https://fakestoreapi.com/products')
+    const products = await response.json()
+
+    this.setState({ loading: false })
+    this.setState({ products })
+  }
+
   addToCart = product => {
     const { cart } = this.state
-    if(cart.find(el => el.name === product.name)){
+
+    const cartProduct = cart.find(
+      element => element.title === product.title
+    )
+
+    if(cartProduct){
       const newCart = cart.map(
-        el => el.name === product.name ? 
-        (
-          {
-            ...el, 
-            amount: el.amount + 1
-          }
-        ) : el
+        element => element.title === product.title 
+        ? (
+            {
+              ...element, 
+              amount: element.amount + 1
+            }
+          ) 
+        : element
       )
+
       return this.setState({ cart: newCart })
     }
+
     return this.setState({
       cart: this.state.cart.concat({
         ...product,
@@ -36,25 +50,30 @@ class App extends Component {
       })
     })
   }
+
   showCart = () => {
     if(!this.state.cart.length) return
+
     this.setState({cartVisible: !this.state.cartVisible})
   }
 
   render() {
-    const { cart, cartVisible, products } = this.state
+    const { cart, cartVisible, products, loading } = this.state
 
     return (
       <div>
         <Navbar 
-        cart={cart} 
-        cartVisible={cartVisible} 
-        showCart={this.showCart}/>
+          cart={cart} 
+          cartVisible={cartVisible} 
+          showCart={this.showCart}
+        />
         <Layout>
           <Title/>
           <Products 
-          products={products}
-          addToCart={this.addToCart}/>
+            loading={loading}
+            products={products}
+            addToCart={this.addToCart}
+          />
         </Layout>
       </div>
     )
